a1105c1b8f50e591c0daa483849f5c0b4ded39e5
ekamal21 <64339149+ekamal21@users.noreply.github.com>
2022-12-23 17:21:38 -0800

    Add project files.

---
+++ Rock  Paper Scissors Game/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Rock__Paper_Scissors_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(471, 205);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please Select Your Choice";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(42, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 69);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Rock";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Aqua;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(215, 278);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 69);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Paper";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Blue;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(383, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 69);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Scissors";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(42, 380);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(216, 66);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Play Again";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(297, 380);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(216, 66);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Exit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 465);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Rock Paper Scissors Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Rock  Paper Scissors Game/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Ezedien kamal 
+namespace Rock__Paper_Scissors_Game
+{
+    public partial class Form1 : Form
+    {
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }   
+        int vall;
+        Random ran = new Random();
+    
+
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+           vall = ran.Next(1, 4);
+
+            label1.Text = "";
+           
+            Scissors();
+            Paper();
+            Rock();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             
+            if (vall  == 1 )
+            {
+               label1.Text = "The Computer Selected Rock: "  + " Its Tie Game";
+            }
+            if (vall == 2 )
+            {
+                label1.Text = "The Computer Selected Paper: " + " The Computer Win";
+            }
+            if (vall == 3 )
+            {
+                label1.Text = "The Computer Selected Scissors: " + " You Win!!";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Paper() == vall)
+            {
+                label1.Text = "The Computer Selected Paper: " + " Its Tie Game";
+            }
+            if (Paper() > vall)
+            {
+                label1.Text = "The Computer Selected Scissors: " + " The Computer Win!";
+            }
+            if (Paper() < vall)
+            {  
+                label1.Text = "The Computer Selected Rock: " + " You Win!!";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Scissors() == vall)
+            {
+                label1.Text = "The Computer Selected Scissors: " + " Its Tie Game";
+            }
+        
+            if (vall == 1)
+            {
+                label1.Text = "The Computer Selected  Rock: " + " The Computer Win";
+            }
+        
+            if (vall == 2)
+            {
+                label1.Text = "The Computer Selected  Paper: " + " You Win!!";
+            }
+
+          
+        }
+        public int Rock()
+        {
+            int Rock = 1;
+            return Rock;
+        }
+        public int Paper()
+        {
+            int Paper = 2;
+            return Paper;
+        }
+        public int Scissors()
+        {
+            int Scissors = 3;
+            return Scissors;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = "Select Your Choice ".ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
